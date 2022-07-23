27ea8dc7719ed0364d0132e54c984bd6af75773e
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 20:35:55 +0530

    Update Vault.sol

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-// All data on-chain can be read! Don't store sensitive information
-// Even if you make it private
-// Level from Ethernaut: https://ethernaut.openzeppelin.com/level/0xf94b476063B6379A3c8b6C836efB8B3e10eDe188
-// Slighly modified
+/// All data on-chain can be read! Don't store sensitive information
+/// Even if you make it private
+/// Level from Ethernaut: https://ethernaut.openzeppelin.com/level/0xf94b476063B6379A3c8b6C836efB8B3e10eDe188
+/// Slighly modified
 
 pragma solidity ^0.8.0;
 
@@ -22,5 +22,3 @@ contract Vault {
         }
     }
 }
-
-// What would you do to exploit this?
