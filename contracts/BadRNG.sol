5a5fdd07b788d52a7434bf29e29e8f84b11cd6ce
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 20:30:33 +0530

    Update BadRNG.sol

--- contracts/BadRNG.sol
+++ contracts/BadRNG.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// One shouldn't use any values from inside the blockchain as randomness
-// Use something like Chainlink VRF for verifiable randomness
-// https://docs.chain.link/docs/get-a-random-number/
+/// One shouldn't use any values from inside the blockchain as randomness
+/// Use something like Chainlink VRF for verifiable randomness
+/// https://docs.chain.link/docs/get-a-random-number/
 
 contract BadRNG {
     address payable[] private s_players;
@@ -17,10 +17,9 @@ contract BadRNG {
         uint256 randomWinnerIndex = uint256(
             keccak256(abi.encodePacked(block.difficulty, msg.sender))
         );
+        
         address winner = s_players[randomWinnerIndex % s_players.length];
         (bool success, ) = winner.call{value: address(this).balance}("");
         require(success, "Transfer failed");
     }
 }
-
-// How could you make a contract that exploits this?
