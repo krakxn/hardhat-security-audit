147bd771e9ba3e8ecb76eec9ba3c606e54476298
AN <108299225+krakxn@users.noreply.github.com>
2022-07-23 20:40:36 +0530

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,18 +6,18 @@ const networkConfig = {
     31337: {
         name: "localhost",
         subscriptionId: "588",
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 Gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: "100000000000000000", // 0.1 ETH
-        callbackGasLimit: "500000", // 500,000 gas
+        callbackGasLimit: "500000", // 500,000 Gas
     },
     4: {
         name: "rinkeby",
         subscriptionId: "588",
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 Gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: "100000000000000000", // 0.1 ETH
-        callbackGasLimit: "500000", // 500,000 gas
+        callbackGasLimit: "500000", // 500,000 Gas
     },
     1: {
         name: "mainnet",
